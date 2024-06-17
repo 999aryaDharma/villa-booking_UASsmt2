9062fb1bed451f27c07337274c908033cfc93f5c
KrisnaMahardika <gedekrisna119@gmail.com>
2024-06-17 22:57:04 +0800

    Update Admin Panel

---
+++ admin-panel/bookings-admin/show-bookings.php
@@ -0,0 +1,38 @@
+<?php
+include_once "../layout/header.php";
+?>
+
+<main class="pl-56 pt-24 pr-9">
+
+    <table class="border-collapse border-2 border-inherit shadow-xl">
+        <thead>
+            <tr> 
+                <th colspan="10" class="text-left border-y-2 border-inherit py-3 pl-3">Bookings</th>
+            </tr>
+            <tr>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Check In</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Check Out</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Email</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Phone Number</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Full Name</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Room Name</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Status</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Payment</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="border-2 border-inherit p-3">11/12/2024</td>
+                <td class="border-2 border-inherit p-3">13/12/2024</td>
+                <td class="border-2 border-inherit p-3">gedekrisna@gmail.com</td>
+                <td class="border-2 border-inherit p-3">081703783931</td>
+                <td class="border-2 border-inherit p-3">I Gede Krisna Mahardika</td>
+                <td class="border-2 border-inherit p-3">Room Aestethic</td>
+                <td class="border-2 border-inherit p-3">Pending</td>
+                <td class="border-2 border-inherit p-3">Rp. 200000</td>
+                <td class="border-2 border-inherit p-3"><a href="" class="inline-block btn bg-yellow-400 hover:bg-orange-600 px-6 py-2 rounded-md text-center pointer-events-auto text-white">Status</a></td>
+            </tr>
+        </tbody>
+    </table>
+</main>
---
+++ admin-panel/rooms-admin.php/show-rooms.php
@@ -0,0 +1,41 @@
+<?php
+include_once "../layout/header.php";
+?>
+
+<main class="pl-56 pt-24 pr-9">
+
+    <table class="border-collapse border-2 border-inherit shadow-xl w-full">
+        <thead>
+            <tr> 
+                <th colspan="8" class="text-left border-y-2 border-inherit py-3 pl-3">Rooms</th>
+                <th colspan="2" class="border-y-2 border-inherit py-3"><a href="../rooms-admin.php/create-rooms.php" class="inline-block btn bg-orange-600 hover:bg-orange-800 px-6 py-2 rounded-md text-center pointer-events-auto text-white">Create Rooms</a></th>
+            </tr>
+            <tr>
+                <th class="border-2 border-inherit px-3 py-1 text-left">#</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Name</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Price</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Num of Persons</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Size</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">View</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Num of Beds</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Status Value</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Edit</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th class="border-2 border-inherit p-3 text-left">1</th>
+                <td class="border-2 border-inherit p-3">Suite Room</td>
+                <td class="border-2 border-inherit p-3">Rp. 2000000</td>
+                <td class="border-2 border-inherit p-3">12</td>
+                <td class="border-2 border-inherit p-3">50</td>
+                <td class="border-2 border-inherit p-3">Sea View</td>
+                <td class="border-2 border-inherit p-3">3</td>
+                <td class="border-2 border-inherit p-3">1</td>
+                <td class="border-2 border-inherit p-3"><a href="edit-rooms.php" class="inline-block btn bg-yellow-400 hover:bg-orange-600 px-6 py-2 rounded-md text-center pointer-events-auto text-white">Edit</a></td>
+                <td class="border-2 border-inherit p-3"><a href="delete-rooms.php" class="inline-block btn bg-red-600 hover:bg-red-800 px-6 py-2 rounded-md text-center pointer-events-auto text-white">Delete</a></td>
+            </tr>
+        </tbody>
+    </table>
+</main>
