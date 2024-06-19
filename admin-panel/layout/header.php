c870a6e20f5e14bf32e5631efaa6b94533a525a3
KrisnaMahardika <gedekrisna119@gmail.com>
2024-06-19 15:55:39 +0800

    fix header

--- admin-panel/layout/header.php
+++ admin-panel/layout/header.php
@@ -1,30 +1,8 @@
 <?php
-<<<<<<< HEAD
-require "../../function.php";
-
-  // Memeriksa apakah pengguna sudah login
-  if (!isset($_SESSION['auth_id'])) {
-    header("location: /auth/login.php");
-    exit();
-  }
-
-  if (!isset($_SESSION['role'])) {
-    echo "Access Denied. You do not have permission to access this page.";
-    exit();
-  }
-
-  // Memeriksa apakah pengguna memiliki peran admin
-  if ($_SESSION ['role'] !== 1) {
-    echo "Access Denied. You do not have permission to access this page.";
-    exit();
-  }
-  // Kode halaman admin di sini...
-
-=======
 require_once "../../function.php";
 $auth_user = getUserById($_SESSION['auth_id'] ?? null);
->>>>>>> e080be2f030866b86ec82d400488bd35be56c155
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
