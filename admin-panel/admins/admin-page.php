f5b49e4271440295a6c0d8daccec81edff5b0229
KrisnaMahardika <gedekrisna119@gmail.com>
2024-06-19 15:57:17 +0800

    fix admin page

--- admin-panel/admins/admin-page.php
+++ admin-panel/admins/admin-page.php
@@ -1,5 +1,6 @@
 <?php
-require "../../function.php";
+  require "../../function.php";
+  include_once "../layout/header.php";
 
   // Memeriksa apakah pengguna sudah login
   if (!isset($_SESSION['auth_id'])) {
@@ -17,35 +18,64 @@ require "../../function.php";
     echo "Access Denied. You do not have permission to access this page.";
     exit();
   }
-  // Kode halaman admin di sini...
+  
+  // Asumsikan $conn adalah objek koneksi mysqli yang sudah terhubung
+  $conn = connect();
+  // Buat query
+  $query = "SELECT users.username, customer.email, users.id 
+            FROM users 
+            JOIN customer ON users.id_customer = customer.id_customer
+            WHERE users.role = 1 ";
 
-  include_once "../layout/header.php";
+  // Eksekusi query
+  $result = $conn->query($query);
 
+  // Periksa apakah ada hasil
+  $rows = [];
+  if ($result && $result->num_rows > 0) {
+      // Ambil semua baris hasil query sebagai objek
+      while ($row = $result->fetch_object()) {
+          $rows[] = $row;
+      }
+  }
 ?>
+
 
 <main class="pl-56 pt-24 pr-9">
 
   <table class="border-collapse border-2 border-inherit shadow-xl w-full">
         <thead>
             <tr> 
-                <th colspan="10" class="text-left border-y-2 border-inherit py-3 pl-3">Admins</th>            
+                <th colspan="5" class="text-left border-y-2 border-inherit py-3 pl-3">Admins</th>            
             </tr>
             <tr>
                 <th class="border-2 border-inherit px-3 py-1 text-left">#</th>
                 <th class="border-2 border-inherit px-3 py-1 text-left">User Name</th>
-                <th class="border-2 border-inherit px-3 py-1 text-left">Email</th>>
-                <th class="border-2 border-inherit px-3 py-1 text-left">Edit</th>
-                <th class="border-2 border-inherit px-3 py-1 text-left">Delete</th>
+                <th class="border-2 border-inherit px-3 py-1 text-left">Email</th>
+                <th class="border-2 border-inherit px-3 py-1 text-center">Edit</th>
+                <th class="border-2 border-inherit px-3 py-1 text-center">Delete</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th class="border-2 border-inherit p-3 text-left">1</th>
-                <td class="border-2 border-inherit p-3">Krisna</td>
-                <td class="border-2 border-inherit p-3">krisna@gmail.com</td>
-                <td class="border-2 border-inherit p-3"><a href="edit-admins.php" class="inline-block btn bg-yellow-400 hover:bg-orange-600 px-6 py-2 rounded-md text-center pointer-events-auto text-white">Edit</a></td>
-                <td class="border-2 border-inherit p-3"><a href="delete-admins.php" class="inline-block btn bg-red-600 hover:bg-red-800 px-6 py-2 rounded-md text-center pointer-events-auto text-white">Delete</a></td>
-            </tr>
+          <?php if (!empty($rows)): ?>
+            <?php foreach ($rows as $admin) : ?>
+              <tr>
+                  <td class="border-2 border-inherit p-3 text-left"> <?= $admin->id ?> </td>
+                  <td class="border-2 border-inherit p-3"><?= $admin->username ?></td>
+                  <td class="border-2 border-inherit p-3"><?= $admin->email ?></td>
+                  <td class="border-2 border-inherit w-24">
+                    <a href="edit-admins.php?id=<?= htmlspecialchars($admin->id_customer) ?>" class="flex justify-center btn bg-yellow-400 hover:bg-orange-600 px-6 py-2 rounded-md text-center text-white">Edit</a>
+                  </td>
+                  <td class="border-2 border-inherit w-24">  
+                    <a href="delete-admins.php?id=<?= htmlspecialchars($admin->id_customer) ?>" class="flex justify-center btn bg-red-600 hover:bg-red-800 px-6 py-2 rounded-md text-center text-white ">Delete</a>
+                  </td> 
+              </tr>
+            <?php endforeach ?>
+          <?php else: ?>
+                <tr>
+                    <td colspan="4" class="border-2 border-inherit p-3 text-center">No data available</td>
+                </tr>  
+          <?php endif ?> 
         </tbody>
-    </table>
+  </table>
 </main>
